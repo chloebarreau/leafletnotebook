3e0eb39acba10226f8196c029a4f5b4d92944f2c
Chloe Barreau <chloeb99@stanford.edu>
2019-10-19 19:18:46 -0700

    button

--- src/App.js
+++ src/App.js
@@ -8,8 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello! This will be the fun abode of Chloe and Grace CS106. 
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <button class="ui button" class="ui blue button">
+          <i class="upload icon"></i> Upload 
+          
+        </button>
+
         <a
           className="App-link"
           href="https://reactjs.org"

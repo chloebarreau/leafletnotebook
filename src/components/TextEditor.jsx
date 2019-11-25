9ab63343ff0d2eb1d44bdb9bfd1c6a2176d800b4
Chloe Barreau <chloeb99@stanford.edu>
2019-11-24 23:39:21 -0800

    text editor with bold and italic

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,31 +1,83 @@
 // Import React!
 import React from 'react'
-import { Form, TextArea } from 'semantic-ui-react'
+import { Form, Button, Segment, Icon } from 'semantic-ui-react'
+import { EditorState, Editor, RichUtils, getDefaultKeyBinding} from 'draft-js'
 
-// Define our app...
+const basicTextStylePlugin = {
+  keyBindingFn(event) {
+    return getDefaultKeyBinding(event);
+  },
+
+  handleKeyCommand(command, { getEditorState, setEditorState }) {
+    const editorState = getEditorState();
+    const newEditorState = RichUtils.handleKeyCommand(
+      editorState, command
+    );
+    if (newEditorState) {
+      setEditorState(newEditorState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+};
+
 class TextEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value: '',
+      editorState: EditorState.createEmpty(),
     };
-
+     /* Create an array of plugins to be passed to `Editor` */
+     this.plugins = [
+      basicTextStylePlugin,
+    ];
+    this.onChange = (editorState) => this.setState({editorState});
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  updateEditorState(editorState){
+    this.setState({editorState});
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
+  _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  }
+
   // Render the editor.
   render() {
     return (
-      <div>
-        
-        <Form>
-          <TextArea style={{ minHeight: 630 }} placeholder='Transcript' value={this.state.value} onChange={this.handleChange}/>
-        </Form>
-      </div>
+      <Segment>
+        <Button icon size='mini' onClick={this._onBoldClick.bind(this)}><Icon name='bold'/></Button>
+        <Button icon size='mini' onClick={this._onItalicClick.bind(this)}><Icon name='italic'/></Button>
+        <div className="editor" onClick={this.focus}>
+          <Editor 
+            editorState={this.state.editorState}
+            onChange={this.updateEditorState.bind(this)}
+            plugins={this.plugins}
+            handleKeyCommand={this.handleKeyCommand}
+            spellCheck={true}
+          ></Editor>
+        </div>
+      </Segment>
     )
   }
 }
